7bf9114779c45141359bcf21a8eda347b6b0a359
Ronnie Henriksen Laugen <rhenri@statoil.com>
2017-05-19 11:00:00 +0200

    Add period picker Only supports month picker atm, but could be extended to a year picker fairly easily

--- ngx-stoui.ts
+++ ngx-stoui.ts
@@ -1,3 +1,4 @@
 export * from './components/sto-calendar/sto-calendar.component';
 export * from './components/sto-dialog/sto-dialog.component';
 export * from './components/sto-button/sto-button.component';
+export * from './components/sto-period/sto-period.component';
