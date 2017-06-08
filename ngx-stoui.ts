7bd3796aa16a6a4c51fc6ef69ee7fa13eccafe13
SBENO <sbeno@statoil.com>
2017-06-08 14:26:56 +0200

    Added navbar to library

--- ngx-stoui.ts
+++ ngx-stoui.ts
@@ -32,4 +32,5 @@ export * from './components/sto-togglebutton/sto-togglebutton.component';
 export * from './components/sto-selectbutton/sto-selectbutton.component';
 export * from './components/sto-spinner/sto-spinner.component';
 export * from './components/sto-messages/sto-messages.component';
+export * from './components/sto-navbar/sto-navbar.component'
 export * from './vendor/primeface/components/common/api';
