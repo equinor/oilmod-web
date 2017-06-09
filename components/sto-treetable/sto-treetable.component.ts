b019c1ac2f9a7ad0457fd7e73c054e693a001241
Ronnie Henriksen Laugen <rhenri@statoil.com>
2017-06-09 16:04:15 +0200

    Attribute for treetable: canSelectChildren [canSelectChildren]="false" will give the ability to select only the top item, and disallow selecting children. Default true

--- components/sto-treetable/sto-treetable.component.ts
+++ components/sto-treetable/sto-treetable.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, ContentChildren, NgModule, QueryList, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ContentChildren, Input, NgModule, QueryList, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoColumn, StoFooter, StoHeader, StoSharedModule } from '../sto-shared/sto-shared';
 import { TreeTable, TreeTableModule } from '../../vendor/primeface/components/treetable/treetable';
@@ -12,6 +12,9 @@ import { StoUITreeRow } from './treetable-row/sto-treetable-row';
     encapsulation: ViewEncapsulation.None
 })
 export class StoTreeTableComponent extends TreeTable {
+
+    @Input() canSelectChildren = true;
+
     @ContentChild(StoHeader) header: StoHeader;
     @ContentChild(StoFooter) footer: StoFooter;
     @ContentChildren(StoColumn) columns: QueryList<StoColumn>;
--- components/sto-treetable/treetable-row/sto-treetable-row.ts
+++ components/sto-treetable/treetable-row/sto-treetable-row.ts
@@ -1,4 +1,4 @@
-import { Component, forwardRef, Inject } from '@angular/core';
+import { Component, forwardRef, Inject, Input } from '@angular/core';
 import { TreeTable, UITreeRow } from '../../../vendor/primeface/components/treetable/treetable';
 import { StoTreeTableComponent } from '../sto-treetable.component';
 
@@ -8,6 +8,17 @@ import { StoTreeTableComponent } from '../sto-treetable.component';
 
 })
 export class StoUITreeRow extends UITreeRow {
+
+  @Input() canSelectChildren: boolean;
+
+  onRowClick(event: MouseEvent) {
+    if (!this.canSelectChildren && !this.parentNode) {
+      this.treeTable.onRowClick(event, this.node);
+    }
+    else if (this.canSelectChildren) {
+      this.treeTable.onRowClick(event, this.node);
+    }
+  }
 
   constructor(@Inject(forwardRef(() => StoTreeTableComponent)) public treeTable: StoTreeTableComponent) {
     super(<TreeTable> treeTable);
