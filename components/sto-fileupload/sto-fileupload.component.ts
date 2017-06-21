038af8eb8d7333b359655b7db8212cb773ad7604
Ronnie Henriksen Laugen <rhenri@statoil.com>
2017-06-21 08:21:16 +0200

    Revert "Dropped files are not uploaded if multiple is set to false"
    This reverts commit 3a980167fccb4532c94c4f3a1922967027483695.

--- components/sto-fileupload/sto-fileupload.component.ts
+++ components/sto-fileupload/sto-fileupload.component.ts
@@ -14,57 +14,6 @@ import { StoSharedModule } from '../sto-shared/sto-shared';
 })
 export class StoFileUploadComponent extends FileUpload {
 
-  upload() {
-
-    if (!this.multiple && this.files.length > 1) {
-      this.onError.emit({ files: this.files, xhr: { statusText: 'Max 1 file', responseText: 'Max 1 file' } });
-      this.files = [];
-      return;
-    }
-
-    this.msgs = [];
-    let xhr = new XMLHttpRequest(),
-      formData = new FormData();
-
-    this.onBeforeUpload.emit({
-      'xhr': xhr,
-      'formData': formData
-    });
-
-    for (let i = 0; i < this.files.length; i++) {
-      formData.append(this.name, this.files[i], this.files[i].name);
-    }
-
-    xhr.upload.addEventListener('progress', (e: ProgressEvent) => {
-      if (e.lengthComputable) {
-        this.progress = Math.round((e.loaded * 100) / e.total);
-      }
-    }, false);
-
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4) {
-        this.progress = 0;
-
-        if (xhr.status >= 200 && xhr.status < 300) {
-          this.onUpload.emit({xhr: xhr, files: this.files});
-        } else {
-          this.onError.emit({xhr: xhr, files: this.files});
-        }
-
-        this.clear();
-      }
-    };
-
-    xhr.open('POST', this.url, true);
-
-    this.onBeforeSend.emit({
-      'xhr': xhr,
-      'formData': formData
-    });
-
-    xhr.send(formData);
-  }
-
 }
 
 
