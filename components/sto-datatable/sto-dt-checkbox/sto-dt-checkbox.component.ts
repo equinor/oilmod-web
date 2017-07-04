fd5128b20866d762818597a27249c4a8c2d3187d
Ronnie Henriksen Laugen <rhenri@statoil.com>
2017-07-04 13:14:50 +0200

    Import css to datatable-checkbox

--- components/sto-datatable/sto-dt-checkbox/sto-dt-checkbox.component.ts
+++ components/sto-datatable/sto-dt-checkbox/sto-dt-checkbox.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { DTCheckbox } from '../../../vendor/primeface/components/datatable/datatable';
 @Component({
   selector: 'sto-dtCheckbox',
-  templateUrl: 'sto-dt-checkbox.component.html'
+  templateUrl: 'sto-dt-checkbox.component.html',
+  styleUrls: ['../../sto-checkbox/sto-checkbox.component.scss']
 })
 export class StoDTCheckbox extends DTCheckbox {
 }
