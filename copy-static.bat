e1c3279f48a1ec046753f3f66d93976c6f84c373
Ronnie <ronnie@rl-web.no>
2017-08-11 10:51:12 +0200

    Small input fix

--- copy-static.bat
+++ copy-static.bat
@@ -1,10 +1,9 @@
-xcopy vendor\bootstrap dist\vendor\bootstrap /s /e /i
-xcopy basics dist\basics /s /e /i
-xcopy vendor\font-awesome dist\vendor\font-awesome /s /e /i
-xcopy components\*.scss dist\components /s /e /i
-xcopy components\*.html dist\components /s /e /i
-xcopy vendor\primeface dist\vendor\primeface /i
+xcopy vendor\bootstrap dist\vendor\bootstrap /s /e /i /q
+xcopy basics dist\basics /s /e /i /q
+xcopy vendor\font-awesome dist\vendor\font-awesome /s /e /i /q
+xcopy components\*.scss dist\components /s /e /i /q
+xcopy components\*.html dist\components /s /e /i /q
+xcopy vendor\primeface dist\vendor\primeface /i /q
 COPY _mixins.scss dist\
 COPY _ngx-stoui.scss dist\
 COPY _variables.scss dist\
-COPY package.dist.json dist\package.json
