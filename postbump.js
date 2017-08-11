928755ede33eab88425ef7e15615d24f068c3b29
Ronnie <ronnie@rl-web.no>
2017-08-11 10:06:58 +0200

    Clean up packages, add postbump script

---
+++ postbump.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+fs.readFile('./package.json', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const package = JSON.parse(data);
+  let newPackage = {
+    "devDependencies": {},
+    "dependencies": {}
+  };
+  newPackage = Object.assign({}, package, newPackage);
+  fs.writeFile('./dist/package.json', JSON.stringify(newPackage), 'utf8', err => {
+    if (err) {
+      throw err;
+    }
+    console.log('New package.json created');
+  })
+});
