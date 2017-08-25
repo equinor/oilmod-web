989273a49c755bd1e46d66d76f54edb6a419859d
Ronnie <ronnie@rl-web.no>
2017-08-25 10:08:44 +0200

    Initial drawer implementation

---
+++ components/sto-drawer/sto-drawer.component.ts
@@ -0,0 +1,124 @@
+import { Component, EventEmitter, Input, NgModule, OnInit, Output, Renderer2 } from '@angular/core';
+import { animate, state, style, transition, trigger, AnimationMetadata } from '@angular/animations';
+
+@Component({
+  selector: 'sto-drawer',
+  templateUrl: './sto-drawer.component.html',
+  styleUrls: ['./sto-drawer.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+        state('in', style({
+          transform: 'translate3d(0, 0, 0)',
+          visibility: 'visible',
+        })),
+        state('outleft', style({
+          transform: 'translate3d(-100%, 0, 0)',
+          visibility: 'hidden',
+        })),
+        state('outright', style({
+          transform: 'translate3d(100%, 0, 0)',
+          visibility: 'hidden',
+        })),
+        transition('in <=> *', animate('400ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
+      ]
+    )
+  ]
+})
+export class StoDrawerComponent implements OnInit {
+  @Input() header: string;
+  @Input() width = '25vw';
+  @Input() padding = '0px';
+  @Input() position: 'left' | 'right';
+  @Input() closeOnClick: boolean;
+
+
+  @Output() onToggle = new EventEmitter<boolean>();
+  @Output() onClose = new EventEmitter();
+  @Output() onOpen = new EventEmitter();
+
+  @Input()
+  get open() {
+    return this._open;
+  }
+
+  set open(open: boolean) {
+    this._open = open;
+    if (this.closeOnClick) {
+      setTimeout(() => {
+        if (open) {
+          this.bindDocumentClickListener();
+        } else if (this.documentClickListener) {
+          this.documentClickListener();
+          this.documentClickListener = null;
+        }
+      });
+    }
+  }
+
+  private _open;
+
+  public toggle() {
+    this.onToggle.emit(!this.open);
+    if (!this.open) {
+      this.openDrawer();
+    } else {
+      this.closeDrawer();
+    }
+  }
+
+  public closeDrawer() {
+    this.open = false;
+    this.onClose.emit();
+  }
+
+  public openDrawer() {
+    this.open = true;
+    this.onOpen.emit();
+  }
+
+  private documentClickListener;
+
+  public style = {
+    [this.position]: this.open ? 0 : `-${this.width}`
+  };
+
+  private bindDocumentClickListener() {
+    if (!this.documentClickListener) {
+      this.documentClickListener = this.renderer.listen('document', 'click', (event) => {
+        let doNothing = false;
+        for (let el of event.path) {
+          if (el.tagName && el.tagName.toLowerCase().match('sidebar')) {
+            doNothing = true;
+            break;
+          }
+        }
+        if (!doNothing && this.open) {
+          this.open = false;
+        }
+      });
+    }
+  }
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngOnInit() {
+    if (!this.position) {
+      this.position = 'left';
+    }
+  }
+}
+
+@Component({
+  selector: 'sto-drawer-wrapper',
+  templateUrl: './sto-drawer-wrapper.component.html',
+  styleUrls: ['./sto-drawer-wrapper.component.scss']
+})
+export class StoDrawerWrapperComponent {}
+
+@NgModule({
+  imports: [],
+  exports: [StoDrawerComponent, StoDrawerWrapperComponent],
+  declarations: [StoDrawerComponent, StoDrawerWrapperComponent]
+})
+export class StoDrawerModule {}
