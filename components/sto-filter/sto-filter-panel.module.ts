b0b483425e667056ff6b77640de5c5101ab8fda0
Sindre Benonisen <sbeno@statoil.com>
2017-10-17 12:35:52 +0200

    GUI fixes in drawer and filter panel

--- components/sto-filter/sto-filter-panel.module.ts
+++ components/sto-filter/sto-filter-panel.module.ts
@@ -1,10 +1,10 @@
 import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdExpansionModule, MdIconModule } from '@angular/material';
+import { MatExpansionModule, MatIconModule, MatButtonModule, MatButtonToggleModule } from '@angular/material';
 import { StoFilterPanelComponent, StoFilterTitle, StoFilterActions,StoFilterTableActions, StoFilterActionsBar } from './sto-filter-panel.component'
 
 @NgModule({
-  imports: [CommonModule, MdExpansionModule, MdIconModule],
+  imports: [CommonModule, MatExpansionModule, MatIconModule, MatButtonModule,MatButtonToggleModule],
   exports: [StoFilterPanelComponent, StoFilterTitle, StoFilterActions,StoFilterTableActions],
   declarations: [StoFilterPanelComponent, StoFilterTitle, StoFilterActions, StoFilterActionsBar,StoFilterTableActions]
 })
