60543706dca96c23877a948037320bc783ea1ae2
Ronnie <ronnie@rl-web.no>
2017-11-06 10:39:17 +0100

    focus first input field

--- components/sto-directives/edit-mode.directive.ts
+++ components/sto-directives/edit-mode.directive.ts
@@ -25,11 +25,16 @@ export class EditModeDirective {
    * Listen for click events on the current element
    * @param {MouseEvent} event
    */
-  @HostListener('click')
+  @HostListener('click', ['$event'])
   onElementClick(event: MouseEvent) {
     this.dontToggle = true;
     this.isEditable = true;
     this.canEdit.emit(true);
+    console.log(event.target['tagName']);
+    const el = event.target['tagName'].toLowerCase();
+    if (el !== 'input' && el !== 'mat-select') {
+      this.focusFirstField();
+    }
   }
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
@@ -37,6 +42,7 @@ export class EditModeDirective {
       this.dontToggle = true;
       this.isEditable = true;
       this.canEdit.emit(true);
+      this.focusFirstField();
     } else if (event.keyCode === 27) {
       this.dontToggle = false;
       this.isEditable = false;
@@ -67,15 +73,39 @@ export class EditModeDirective {
     let shouldClose = path.filter(e => e.tagName)
       .filter(e => {
         return this.dontToggleElements.indexOf(e.tagName.toLowerCase()) >= 0;
-      })
-      .map(e =>  Array.from(e.classList))
-      .reduce((a, b) => [...a, ...b], [])
-      .filter(c =>  this.dontToggleElements.indexOf(c) < 0)
-      .length === 0;
+      }).length === 0;
+    if (shouldClose) {
+      shouldClose = path.filter(e => e.tagName)
+        .map(e =>  Array.from(e.classList))
+        .reduce((a, b) => [...a, ...b], [])
+        .filter(c => this.dontToggleElements.indexOf(c) >= 0).length === 0;
+    }
     if (shouldClose) {
         this.isEditable = false;
         this.canEdit.emit(false);
     }
+  }
+
+  private focusFirstField() {
+    setTimeout(() => {
+      const self = this.el.nativeElement;
+      const inputs = Array.from(self.getElementsByTagName('input'));
+      const textAreas = Array.from(self.getElementsByTagName('textarea'));
+      const selects = Array.from(self.getElementsByTagName('mat-select'));
+      const allInputs = [...inputs, ...selects, ...textAreas]
+        .filter((el: HTMLInputElement) => !(el.readOnly))
+        .sort((a, b) => {
+        if (a === b) {
+          return 0;
+        } else if (a.compareDocumentPosition(b) & 2) {
+          return 1;
+        }
+        return -1;
+      });
+      if (allInputs[0]) {
+        allInputs[0].focus();
+      }
+    }, 0);
   }
 
   constructor(private el: ElementRef) {
