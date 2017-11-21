5451dc531d3eddc427bbc9bb459680c779d9ee4b
Sindre Benonisen <sbeno@statoil.com>
2017-11-21 08:50:58 +0100

    Added simple footer

---
+++ components/sto-action-footer/sto-action-footer.component.ts
@@ -0,0 +1,27 @@
+import { ViewChild, AfterViewInit, Component, Directive, EventEmitter, Input, OnInit, Output, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
+import { Renderer2, OnDestroy} from '@angular/core';
+
+
+@Component({
+  selector: 'sto-action-footer',
+  templateUrl: './sto-action-footer.component.html',
+  styleUrls : ['./sto-action-footer.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'sto-action-footer'
+  }
+})
+export class StoActionFooterComponent implements OnInit, OnDestroy  {
+  ngOnDestroy(): void {
+    this.renderer.removeClass(document.body, 'sto-has-action-footer');
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  constructor(private renderer: Renderer2) {
+    this.renderer.addClass(document.body, 'sto-has-action-footer');
+  }
+
+}
