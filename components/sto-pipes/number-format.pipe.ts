2f61ed4ce70b63e71f5b44e69583fd033bc49eea
Ronnie <ronnie@rl-web.no>
2017-12-01 12:00:24 +0100

    Fix decimal setup in number-format-pipe

--- components/sto-pipes/number-format.pipe.ts
+++ components/sto-pipes/number-format.pipe.ts
@@ -20,11 +20,12 @@ export class NumberFormatPipe implements PipeTransform {
     }
     const intl = new Intl.NumberFormat().format(value);
     const split = intl.split('.');
+    let localized = split[0];
     if(appendDecimals){
       const decimals = split.length === 2 ? split[1] : '';
       split[1] = decimals.padEnd(3, '0');
+      localized = split.join('.');
     }
-    const localized = split.join('.');
     return localized.replace(/,/g, ' ').replace('.', ',') + ` ${unit}`;
   }
 }
