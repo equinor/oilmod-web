abd4ceab07cc50b511fed6e41d978dcf923d4be9
Sindre Benonisen <sbeno@statoil.com>
2018-01-04 10:21:49 +0100

    Added loading indicator to footer

--- components/sto-action-footer/sto-action-footer.module.ts
+++ components/sto-action-footer/sto-action-footer.module.ts
@@ -1,10 +1,10 @@
 import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatExpansionModule, MatIconModule, MatButtonModule, MatButtonToggleModule } from '@angular/material';
+import { MatExpansionModule, MatIconModule, MatButtonModule, MatButtonToggleModule, MatProgressBarModule } from '@angular/material';
 import { StoActionFooterComponent } from './sto-action-footer.component';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatButtonToggleModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatButtonToggleModule, MatProgressBarModule],
   exports: [StoActionFooterComponent],
   declarations: [StoActionFooterComponent]
 })
