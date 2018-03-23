cec130adfb6e20b374c11e9e534f000ff084db1a
Sindre Benonisen <sbeno@statoil.com>
2018-03-23 09:18:55 +0100

    Fixed number and yes/no pipe

--- components/sto-pipes/number-format.pipe.ts
+++ components/sto-pipes/number-format.pipe.ts
@@ -1,10 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * Rounds numbers
+ */
 @Pipe({
   name: 'numberFormat'
 })
 export class NumberFormatPipe implements PipeTransform {
-  transform(value: number, unit: string = '', abs?: boolean, appendDecimals = true): string {
+
+  transform(value: number, unit: string = '', abs?: boolean, appendDecimals = true, numberOfDecimals = 3): string {
     if (value !== 0 && !value) {
       return value as any;
     }
@@ -15,20 +19,40 @@ export class NumberFormatPipe implements PipeTransform {
       }
       value = newValue;
     }
-    if (abs) {
-      value = Math.abs(value);
+    // We absolute the value to ensure that the rounding rules is always away from zero.
+    // 1.5 => 2 and -1.5 => -2
+    let isNegativeNumber = value < 0;
+    value = Math.abs(value);
+
+    if(!appendDecimals){
+      value = Math.round(value);
     }
-    if (!isNaN(value)) {
-      value = parseFloat(value.toFixed(3));
+    if (!isNaN(value) && appendDecimals) {
+      value = parseFloat(this.toFixed(value, numberOfDecimals));
     }
+    //Turn negative numbers back
+    if (isNegativeNumber) {
+      value = value*-1;
+    }
+    let localized = this.prettyPrintValue(value, appendDecimals,numberOfDecimals);
+    return localized.replace(/,/g, ' ').replace('.', ',') + ` ${unit}`;
+  }
+
+  private prettyPrintValue(value: number, appendDecimals: boolean, numberOfDecimals) {
     const intl = new Intl.NumberFormat().format(value);
     const split = intl.split('.');
     let localized = split[0];
-    if(appendDecimals){
+
+    if (appendDecimals) {
       const decimals = split.length === 2 ? split[1] : '';
-      split[1] = decimals.padEnd(3, '0');
+      split[1] = decimals.padEnd(numberOfDecimals, '0');
       localized = split.join('.');
     }
-    return localized.replace(/,/g, ' ').replace('.', ',') + ` ${unit}`;
+    return localized;
+  }
+
+  //Normal toFixed has some issues: https://stackoverflow.com/questions/10015027/javascript-tofixed-not-rounding
+  private toFixed( num, precision ) {
+    return (+(Math.round(+(num + 'e' + precision)) + 'e' + -precision)).toFixed(precision);
   }
 }
--- components/sto-pipes/yes-no.ts
+++ components/sto-pipes/yes-no.ts
@@ -1,8 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'yesNo'})
 export class YesNoPipe implements PipeTransform {
-  transform(value: boolean): string {
-    return value ? 'Yes' : 'No';
+  transform(value: boolean, ignoreNulls = false): string {
+    let valueStr = '';
+    if(ignoreNulls){
+      valueStr = value ? 'Yes' : value === false ? 'No' : null;
+    }
+    else{
+      valueStr = value ? 'Yes' : 'No';
+    }
+    return valueStr;
 
   }
 }
