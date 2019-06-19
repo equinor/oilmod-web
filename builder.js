508af4316f1b1a713e68fa6b9a8d52dab8d9cd44
Ronnie <rhenri@statoil.com>
2019-06-19 08:50:40 +0200

    Update builder to include scripts, and add license field

--- builder.js
+++ builder.js
@@ -19,11 +19,37 @@ runner()
   .catch(console.error);
 
 async function runner() {
-  await build('stoui-core');
-  await runGulpTask();
-  await buildOthers();
-  await buildLast();
-  await sharedFormCss();
+  await bumpVersions();
+  /*  await build('stoui-core');
+    await runGulpTask();
+    await buildOthers();
+    await buildLast();
+    await sharedFormCss();*/
+}
+
+async function bumpVersions() {
+  const shouldBump = process.argv.includes('--bump');
+  if (shouldBump) {
+    const i = process.argv.indexOf('--bump');
+    const type = process.argv[i + 1];
+    if (!['major', 'minor', 'patch'].includes(type)) {
+      throw new Error(`Invalid version bump. Expected 'major', 'minor', 'patch' but got ${type}`);
+    }
+    return await Promise.all(['stoui-core', ...others, ...last].map(lib => bumpLib(lib, type)));
+  }
+  return true;
+}
+
+async function bumpLib(lib, increment) {
+  console.log(`Bumping ${lib} by one ${increment}`);
+  const {stdout, stderr} = await exec(`cd ${path.join(__dirname, 'projects', lib)} && yarn version --no-git-tag-version --${increment} && cd ${__dirname}`);
+  if (stderr) {
+    console.error(`Failed bumping ${lib}`);
+    console.error(`Stacktrace: `, stderr);
+    throw new Error('Exiting due to build failure');
+  }
+  console.log(stdout);
+  return true;
 }
 
 async function buildOthers() {
@@ -41,7 +67,7 @@ async function sharedFormCss() {
 async function build(lib) {
   console.log(`Building ${lib}...`);
   const {stdout, stderr} = await exec(`${path.normalize('./node_modules/.bin/ng')} build ${lib}`);
-  if (stderr) {
+  if (stderr && !/caniuse/.test(stderr)) {
     console.error(`Failed building ${lib}`);
     console.error(`Stacktrace: `, stderr);
     throw new Error('Exiting due to build failure');
