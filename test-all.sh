a09b3102ab2485e000bcaae8b6cd821e8d3cc59e
Ronnie <ronnie@rlaugen.no>
2019-12-17 10:27:40 +0100

    Run tests in parallel

--- test-all.sh
+++ test-all.sh
@@ -3,8 +3,12 @@
 node builder.js --project core
 node builder.js --project form
 
-yarn test stoui-core
-yarn test stoui-common
-yarn test stoui-datatable
-yarn test stoui-drawer
+yarn test stoui-core &
+yarn test stoui-common &
+yarn test stoui-datatable &
+yarn test stoui-drawer &
 yarn test stoui-form
+
+wait
+
+echo "Test suite completed"
