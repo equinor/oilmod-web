a63d930aa65e57451e89777cadb732633b07de56
Ronnie <ronnie@rlaugen.no>
2019-12-20 08:13:11 +0100

    Will bash cause test failure?

--- test-all.sh
+++ test-all.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-node builder.js --project core
-node builder.js --project form
+#node builder.js --project core
+#node builder.js --project form
 
-yarn test stoui-core &
-yarn test stoui-common
-
-wait
-
-
-yarn test stoui-datatable &
-yarn test stoui-form
-wait
+yarn test stoui-core
+#yarn test stoui-common
+#yarn test stoui-datatable
+#yarn test stoui-form
 
 echo "Test suite completed"
