125a22a5fe2cb6d26221ce4248104b8c035a9b5b
Ronnie <ronnie@rlaugen.no>
2020-03-13 14:09:43 +0100

    Export all the things

--- projects/stoui-error-handler/src/public_api.ts
+++ projects/stoui-error-handler/src/public_api.ts
@@ -6,5 +6,4 @@ export * from './lib/sto-http-error-handler/format-error-message';
 export * from './lib/sto-http-error-handler/http-error-handler.module';
 export * from './lib/sto-http-error-handler/http-error-handler.service';
 export * from './lib/sto-http-error-handler/unexcepted-dialog/exception-dialog.component';
-export * from './lib/error-handler/error-handler.module';
-export * from './lib/error-handler/token';
+export * from './lib/error-handler';
