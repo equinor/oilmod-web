eb845ffa701de4911334e6157e3f0fda12da6ae1
Ronnie <ronnie@rlaugen.no>
2020-03-16 10:06:09 +0100

    Delete barrel file - thought that was fixed but no cigar.

--- projects/stoui-error-handler/src/lib/error-handler/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './error-dialog.service';
-export * from './error-handler';
-export * from './error-handler.service';
-export * from './error-handler.module';
-export * from './http-error';
-export * from './token';
--- projects/stoui-error-handler/src/public_api.ts
+++ projects/stoui-error-handler/src/public_api.ts
@@ -6,4 +6,10 @@ export * from './lib/sto-http-error-handler/format-error-message';
 export * from './lib/sto-http-error-handler/http-error-handler.module';
 export * from './lib/sto-http-error-handler/http-error-handler.service';
 export * from './lib/sto-http-error-handler/unexcepted-dialog/exception-dialog.component';
-export * from './lib/error-handler';
+export * from './lib/error-handler/error-dialog.service';
+export * from './lib/error-handler/error-handler';
+export * from './lib/error-handler/error-handler.service';
+export * from './lib/error-handler/error-handler.module';
+export * from './lib/error-handler/http-error';
+export * from './lib/error-handler/token';
+
