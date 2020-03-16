c8b8842ef1f76189472b7f7b9571f98628f1bc03
Ronnie <ronnie@rlaugen.no>
2020-03-16 10:18:59 +0100

    Drop beta + close ref to avoid layered errors

--- projects/stoui-error-handler/src/lib/error-handler/error-dialog.service.ts
+++ projects/stoui-error-handler/src/lib/error-handler/error-dialog.service.ts
@@ -7,18 +7,23 @@ import { DialogComponent } from './dialog/dialog.component';
   providedIn: 'root'
 })
 export class ErrorDialogService {
+  private ref: MatDialogRef<DialogComponent>;
 
   constructor(private dialog: MatDialog) {
   }
 
   // Opens a dialog, and returns the reference with the close results.
   open(err: HttpError): MatDialogRef<DialogComponent, any> {
-    return this.dialog.open(DialogComponent, {
+    if ( this.ref ) {
+      this.ref.close(null);
+    }
+    this.ref = this.dialog.open(DialogComponent, {
       data: err,
       width: '560px',
       autoFocus: false,
       disableClose: true,
       panelClass: 'sto-dialog'
     });
+    return this.ref;
   }
 }
