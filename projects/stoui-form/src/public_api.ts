26ef92c67ce61bdfcd7434cbbaafea61d3950d05
Ronnie <rhenri@equinor.com>
2020-03-29 20:30:12 +0200

    Form (also unsure)

--- projects/stoui-form/src/public_api.ts
+++ projects/stoui-form/src/public_api.ts
@@ -3,9 +3,13 @@
  */
 
 export * from './lib/sto-autocomplete/sto-autocomplete.module';
+export * from './lib/sto-autocomplete/sto-autocomplete.component';
 export * from './lib/sto-daterange/sto-daterange.module';
+export * from './lib/sto-daterange/sto-daterange.component';
 export * from './lib/sto-inline-calendar/sto-inline-calendar.module';
-export * from './lib/sto-monthpicker/datepicker-module';
+export * from './lib/sto-inline-calendar/sto-inline-calendar';
+export * from './lib/sto-inline-calendar/sto-inline-calendar-month-view';
+export * from './lib/sto-inline-calendar/sto-inline-calendar-body';
 export * from './lib/sto-number-input/sto-number-input.component';
 export * from './lib/sto-number-input/sto-number-input.directive';
 export * from './lib/sto-number-input/sto-number-input.module';
@@ -14,4 +18,19 @@ export * from './lib/sto-select-filter/sto-select-filter.component';
 export * from './lib/sto-select-filter/sto-select-filter.module';
 export * from './lib/sto-slide-toggle/sto-slide-toggle.component';
 export * from './lib/sto-form/sto-form.module';
+export * from './lib/sto-form/form-field.directive';
 export * from './lib/sto-wysiwyg/wysiwyg.module';
+export * from './lib/sto-wysiwyg/wysiwyg.component';
+export * from './lib/sto-monthpicker/calendar-body';
+export * from './lib/sto-monthpicker/calendar';
+export * from './lib/sto-monthpicker/coerce-date-property';
+export * from './lib/sto-monthpicker/datepicker-errors';
+export * from './lib/sto-monthpicker/datepicker-input';
+export * from './lib/sto-monthpicker/datepicker-intl';
+export * from './lib/sto-monthpicker/datepicker-module';
+export * from './lib/sto-monthpicker/datepicker-toggle';
+export * from './lib/sto-monthpicker/datepicker';
+export * from './lib/sto-monthpicker/month-view';
+export * from './lib/sto-monthpicker/public-api';
+export * from './lib/sto-monthpicker/sto-monthformfield-click.directive';
+export * from './lib/sto-monthpicker/year-view';
