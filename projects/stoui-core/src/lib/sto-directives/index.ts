b2dee6aacf815af9b383d4f79f21fe333c1f7f98
Ronnie <rhenri@equinor.com>
2020-03-29 15:35:14 +0200

    add barrel for directives

---
+++ projects/stoui-core/src/lib/sto-directives/index.ts
@@ -0,0 +1,7 @@
+export * from './quick-keys.directive';
+export * from './sto-ignore-menu-backdrop.directive';
+export * from './sto-ignore-contextmenu-backdrop.directive';
+export * from './menu-focus.directive';
+export * from './date-form-field-click.directive';
+export * from './sto-select-text-on-focus.directive';
+export * from './sto-grid.directive';
