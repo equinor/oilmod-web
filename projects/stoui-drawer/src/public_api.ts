e25a83aeabfdc25b8cede866b7dfa9b630c25e96
Ronnie <rhenri@equinor.com>
2020-03-29 20:23:21 +0200

    Drawer

--- projects/stoui-drawer/src/public_api.ts
+++ projects/stoui-drawer/src/public_api.ts
@@ -6,3 +6,6 @@ export * from './lib/sto-drawer/sto-drawer.component';
 export * from './lib/sto-drawer/sto-drawer.module';
 export * from './lib/sto-navigation/sto-navigation.module';
 export * from './lib/sto-navigation/tokens';
+export * from './lib/sto-navigation/navigation-part/navigation-part.component';
+export * from './lib/sto-navigation/navigation-menu/navigation-menu.component';
+export * from './lib/sto-navigation/navigation-menu/navigation-menu-item/navigation-menu-item.component';
