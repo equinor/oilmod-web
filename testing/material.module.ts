29165289c41554d2a87b67617ec0aad311d124cf
Ronnie Laugen <rhenri@equinor.com>
2020-12-22 08:45:27 +0100

    Add primary hues as well, plus fix table spec

--- testing/material.module.ts
+++ testing/material.module.ts
@@ -7,7 +7,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
@@ -75,6 +75,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatTreeModule,
     BrowserAnimationsModule,
     MatNativeDateModule,
+    MatRippleModule
   ],
   exports: [
     MatDatepickerModule,
@@ -115,6 +116,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatNativeDateModule,
     FormsModule,
     ReactiveFormsModule,
+    MatRippleModule
   ]
 })
 export class MaterialModule {
