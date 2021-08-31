964661cc9305af64f9ef2140ab7ab92d2c53ecef
Ronnie Laugen <rhenri@equinor.com>
2021-08-31 08:15:48 +0200

    [GOM] Adapt to header

--- libs/drawer/src/lib/nav-drawer/nav-drawer.component.ts
+++ libs/drawer/src/lib/nav-drawer/nav-drawer.component.ts
@@ -1,16 +1,32 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewEncapsulation } from '@angular/core';
 import { Navigation } from './navigation';
+import { StoThemeService } from '@ngx-stoui/common';
 
 @Component({
   selector: 'sto-nav-drawer',
   templateUrl: './nav-drawer.component.html',
-  styleUrls: ['./nav-drawer.component.scss'],
+  styleUrls: [ './nav-drawer.component.scss' ],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavDrawerComponent {
+export class NavDrawerComponent implements AfterViewInit {
   @Input()
   collapsed = false;
   @Input()
   navigationItems: Array<Navigation>;
+  @Input()
+  withAppHeader: boolean;
+  public headerOffset = 64;
+
+  constructor(private themeService: StoThemeService, private cdr: ChangeDetectorRef) {
+  }
+
+  ngAfterViewInit() {
+    this.themeService
+      .getActiveTypography()
+      .subscribe(typography => {
+        this.headerOffset = typography.name === 'small' ? 48 : 64;
+        this.cdr.detectChanges();
+      });
+  }
 }
--- libs/drawer/src/lib/nav-drawer/nav-drawer.stories.ts
+++ libs/drawer/src/lib/nav-drawer/nav-drawer.stories.ts
@@ -3,11 +3,12 @@ import { moduleMetadata } from '@storybook/angular';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { NavDrawerModule, NavDrawerComponent } from '@ngx-stoui/drawer';
+import { NavDrawerComponent, NavDrawerModule } from '@ngx-stoui/drawer';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { StoAppHeaderModule } from '@ngx-stoui/common';
 
 @Component({
   selector: 'wrapper',
@@ -25,6 +26,7 @@ export default {
       declarations: [WrapperComponent],
       imports: [
         NavDrawerModule,
+        StoAppHeaderModule,
         MatIconModule,
         MatButtonModule,
         BrowserAnimationsModule,
@@ -50,6 +52,57 @@ const Template: Story<NavDrawerComponent> = (args: NavDrawerComponent) => {
   };
 };
 
+const navigationItems = [
+  {
+    label: 'Internal route title',
+    children: [
+      {
+        route: [ '/', 'route1' ],
+        label: 'Internal child route',
+      },
+      {
+        route: [ '/', 'route2' ],
+        label: 'Internal child route 2',
+      },
+      {
+        route: [ '/', 'route3' ],
+        label: 'Internal child route 3',
+      },
+      {
+        route: [ '/', 'route4' ],
+        label: 'Internal child route 4',
+      },
+      {
+        route: [ '/', 'route5' ],
+        label: 'Internal child route 5',
+      },
+      {
+        route: [ '/', 'route6' ],
+        label: 'Internal child route 6',
+      },
+    ],
+    icon: 'home'
+  },
+  {
+    label: 'External',
+    children: [
+      {
+        link: 'https://example.com',
+        target: '_blank',
+        label: 'Example.com'
+      }
+    ],
+    icon: 'open_in_new',
+  },
+  {
+    link: '#',
+    label: 'Disabled Route',
+    children: [],
+    icon: 'do_disturb',
+    disabled: true
+  },
+];
+
 export const NormalUse = (args: NavDrawerComponent) => {
   return {
     component: NavDrawerComponent,
@@ -57,57 +110,25 @@ export const NormalUse = (args: NavDrawerComponent) => {
   };
 };
 NormalUse.args = {
-  navigationItems: [
-    {
-      label: 'Internal route title',
-      children: [
-        {
-          route: ['/', 'route1'],
-          label: 'Internal child route',
-        },
-        {
-          route: ['/', 'route2'],
-          label: 'Internal child route 2',
-        },
-        {
-          route: ['/', 'route3'],
-          label: 'Internal child route 3',
-        },
-        {
-          route: ['/', 'route4'],
-          label: 'Internal child route 4',
-        },
-        {
-          route: ['/', 'route5'],
-          label: 'Internal child route 5',
-        },
-        {
-          route: ['/', 'route6'],
-          label: 'Internal child route 6',
-        },
-      ],
-      icon: 'home'
-    },
-    {
-      label: 'External',
-      children: [
-        {
-          link: 'https://example.com',
-          target: '_blank',
-          label: 'Example.com'
-        }
-      ],
-      icon: 'open_in_new',
-    },
-    {
-      link: '#',
-      label: 'Disabled Route',
-      children: [],
-      icon: 'do_disturb',
-      disabled: true
-    },
-  ],
+  height: '100vh',
+  navigationItems,
   collapsed: false
+};
+
+
+export const WithAppHeader = (args: NavDrawerComponent) => {
+  return {
+    component: NavDrawerComponent,
+    props: args,
+    template: `
+    <sto-app-header></sto-app-header>
+    <sto-nav-drawer [withAppHeader]="true" [navigationItems]="navigationItems" [collapsed]="collapsed"></sto-nav-drawer>
+    `
+  };
+};
+WithAppHeader.args = {
+  navigationItems,
+  collapsed: true,
 };
 
 
