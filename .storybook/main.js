749b8e0a0bd0d63892564346edcdecf64952b409
Ronnie Laugen <rhenri@equinor.com>
2021-11-30 12:11:50 +0100

    post-nx-migrate (v3) update scss++

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,10 @@ module.exports = {
   stories: [],
   // addons: ['@storybook/addon-knobs/dist/register'],
   addons: [
+    "@storybook/addon-actions",
     '@storybook/addon-essentials',
-    // "@storybook/addon-actions",
-    '@storybook/addon-essentials',
-    // "@storybook/addon-storysource",
-    // "@storybook/addon-links",
-    'storybook-dark-mode',
+    "@storybook/addon-storysource",
+    "@storybook/addon-links",
+    // 'storybook-dark-mode',
   ],
 };
