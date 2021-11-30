42c86c62c521315400535b2514ed27264c4c9e71
Ronnie Laugen <rhenri@equinor.com>
2021-11-30 09:13:48 +0100

    after npm install, pre-migrate

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,10 @@ module.exports = {
   stories: [],
   // addons: ['@storybook/addon-knobs/dist/register'],
   addons: [
-    "@storybook/addon-actions",
+    // "@storybook/addon-actions",
     "@storybook/addon-essentials",
-    "@storybook/addon-storysource",
-    "@storybook/addon-links",
+    // "@storybook/addon-storysource",
+    // "@storybook/addon-links",
     'storybook-dark-mode',
   ]
 };
