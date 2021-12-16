4a8ee76d35b2c63723411d52ad9ac5f5a488afb2
Ronnie Laugen <rhenri@equinor.com>
2021-12-16 10:32:14 +0100

    More storybook-fixes (add theming toolbar)

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: [],
   // addons: ['@storybook/addon-knobs/dist/register'],
   addons: [
+    "@storybook/addon-docs",
     '@storybook/addon-essentials',
     "@storybook/addon-storysource",
     "@storybook/addon-links",
