1cdccb25b881c298443e15b41544facb3145feb3
Ronnie Laugen <rhenri@equinor.com>
2021-12-16 08:57:06 +0100

    Remove storybook-deps

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ module.exports = {
   stories: [],
   // addons: ['@storybook/addon-knobs/dist/register'],
   addons: [
-    "@storybook/addon-actions",
     '@storybook/addon-essentials',
     "@storybook/addon-storysource",
     "@storybook/addon-links",
