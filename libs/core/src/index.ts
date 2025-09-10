49328a840a2d90da25311f401e115cbb79dd395f
Øystein Amundsen <oyamu@equinor.com>
2025-09-10 15:03:45 +0200

    fix: style fixes

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -3,9 +3,8 @@
  */
 
 export * from './lib/abstract-and-interfaces/keyPress.enum';
-export * from './lib/sto-directives/directives.module';
 export * from './lib/sto-directives';
-export * from './lib/sto-pipes/sto-pipes.module';
+export * from './lib/sto-directives/directives.module';
 
 export * from './lib/sto-pipes/currency-format.pipe';
 export * from './lib/sto-pipes/date-format.pipe';
--- libs/core/src/lib/sto-directives/directives.module.ts
+++ libs/core/src/lib/sto-directives/directives.module.ts
@@ -1,9 +1,13 @@
-import { QuickKeysDirective } from './quick-keys.directive';
-import { DateFormFieldClickDirective } from './date-form-field-click.directive';
-import { StoSelectTextOnFocusDirective } from './sto-select-text-on-focus.directive';
-import { StoGridColumnDirective, StoGridDirective, StoGridSpacerDirective } from './sto-grid.directive';
-import { MenuOverlayDirective } from './menu-overlay.directive';
 import { ContextMenuDirective } from './context-menu.directive';
+import { DateFormFieldClickDirective } from './date-form-field-click.directive';
+import { MenuOverlayDirective } from './menu-overlay.directive';
+import { QuickKeysDirective } from './quick-keys.directive';
+import {
+  StoGridColumnDirective,
+  StoGridDirective,
+  StoGridSpacerDirective,
+} from './sto-grid.directive';
+import { StoSelectTextOnFocusDirective } from './sto-select-text-on-focus.directive';
 
 /**
  * Directives have been made standalone, import them directly:
@@ -20,25 +24,24 @@ import { NgModule } from '@angular/core';
 
 @NgModule({
   imports: [
-    QuickKeysDirective
-    , DateFormFieldClickDirective
-    , StoSelectTextOnFocusDirective
-    , StoGridDirective
-    , StoGridColumnDirective
-    , StoGridSpacerDirective
-    , MenuOverlayDirective
-    , ContextMenuDirective
+    QuickKeysDirective,
+    DateFormFieldClickDirective,
+    StoSelectTextOnFocusDirective,
+    StoGridDirective,
+    StoGridColumnDirective,
+    StoGridSpacerDirective,
+    MenuOverlayDirective,
+    ContextMenuDirective,
   ],
   exports: [
-    QuickKeysDirective
-    , DateFormFieldClickDirective
-    , StoSelectTextOnFocusDirective
-    , StoGridDirective
-    , StoGridColumnDirective
-    , StoGridSpacerDirective
-    , MenuOverlayDirective
-    , ContextMenuDirective
-  ]
+    QuickKeysDirective,
+    DateFormFieldClickDirective,
+    StoSelectTextOnFocusDirective,
+    StoGridDirective,
+    StoGridColumnDirective,
+    StoGridSpacerDirective,
+    MenuOverlayDirective,
+    ContextMenuDirective,
+  ],
 })
-export class StoDirectivesModule {
-}
+export class StoDirectivesModule {}
